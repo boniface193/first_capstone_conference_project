46791bbdf9ac62f5600f7105c25045f8c41480c8
deboik193 <deboik193@gmail.com>
2023-01-11 03:52:42 +0100

    featured speakers added as an object

---
+++ js/featured.js
@@ -0,0 +1,47 @@
+const featured_Speaker = [
+  {
+    chessboard: './assets/images/Chess_Board.png',
+    img: './assets/images/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpg',
+    fullname: 'Yoachi Davis',
+    text: 'Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Optio, nemo!',
+    text2: 'Lorem ipsum dolor, sit amet consec tetur adipi sicing elit. Perfe rendis, distin ctio.'
+  },
+  {
+    chessboard: './assets/images/Chess_Board.png',
+    img: './assets/images/shipman-northcutt-sgZX15Da8YE-unsplash.jpg',
+    fullname: 'Yoachi Davis',
+    text: 'Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Optio, nemo!',
+    text2: 'Lorem ipsum dolor, sit amet consec tetur adipi sicing elit. Perfe rendis, distin ctio.'
+  },
+  {
+    chessboard: './assets/images/Chess_Board.png',
+    img: './assets/images/icons8-team-FcLyt7lW5wg-unsplash.jpg',
+    fullname: 'Yoachi Davis',
+    text: 'Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Optio, nemo!',
+    text2: 'Lorem ipsum dolor, sit amet consec tetur adipi sicing elit. Perfe rendis, distin ctio.'
+  },
+  {
+    chessboard: './assets/images/Chess_Board.png',
+    img: './assets/images/m-brauer-yEYmnaFuYVM-unsplash.jpg',
+    fullname: 'Yoachi Davis',
+    text: 'Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Optio, nemo!',
+    text2: 'Lorem ipsum dolor, sit amet consec tetur adipi sicing elit. Perfe rendis, distin ctio.'
+  },
+  {
+    chessboard: './assets/images/Chess_Board.png',
+    img: './assets/images/ph-m-duy-quang-QGr6H7pri-Q-unsplash.jpg',
+    fullname: 'Yoachi Davis',
+    text: 'Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Optio, nemo!',
+    text2: 'Lorem ipsum dolor, sit amet consec tetur adipi sicing elit. Perfe rendis, distin ctio.'
+  },
+  {
+    chessboard: './assets/images/Chess_Board.png',
+    img: './assets/images/m-brauer-W49xnqoy6oc-unsplash.jpg',
+    fullname: 'Yoachi Davis',
+    text: 'Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Optio, nemo!',
+    text2: 'Lorem ipsum dolor, sit amet consec tetur adipi sicing elit. Perfe rendis, distin ctio.'
+  },
+
+]
+
+export default featured_Speaker
---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+function sideBarFun() {
+  const openBar = document.querySelectorAll('#openBar')
+  const sideBar = document.querySelector('#sideBar');
+  const navLink = document.querySelector('#navLink');
+  const navLinkLi = navLink.querySelectorAll(':scope > aside > div');
+  function openAndCloseBar() {
+    navLink.classList.toggle('d-none')
+    sideBar.classList.toggle('d-none');
+  }
+
+  openBar.forEach((i) => i.addEventListener('click', openAndCloseBar));
+  navLinkLi.forEach((i) => i.addEventListener('click', openAndCloseBar));
+}
+
+export default sideBarFun;
